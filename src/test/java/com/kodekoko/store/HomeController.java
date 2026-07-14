8cb66ba1db9ecf41bd55207b7ac6473c65347650
Keyur Kulkarni <163241275+Keyur-Hat@users.noreply.github.com>
2026-07-14 17:48:10 +0530

    Springboot Refresh

---
+++ src/test/java/com/kodekoko/store/HomeController.java
@@ -0,0 +1,9 @@
+package com.kodekoko.store;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+    
+}
---
+++ src/test/java/com/kodekoko/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodekoko.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
