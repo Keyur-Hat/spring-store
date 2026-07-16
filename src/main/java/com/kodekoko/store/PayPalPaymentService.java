2c749ac0809855c46d7e106dcdeba90d2676294e
Keyur Kulkarni <163241275+Keyur-Hat@users.noreply.github.com>
2026-07-16 18:30:31 +0530

    Dependancy Injection : Interface

---
+++ src/main/java/com/kodekoko/store/PayPalPaymentService.java
@@ -0,0 +1,9 @@
+package com.kodekoko.store;
+
+public class PayPalPaymentService implements PaymentService {
+
+    @Override
+    public void processPayment(double amount) {
+        
+    }
+}
---
+++ src/main/java/com/kodekoko/store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.kodekoko.store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
